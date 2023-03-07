cbe7ef7f3072f9a112f38830e220189e32d22d92
Ali Waqar <aliwaqar981@gmail.com>
2023-03-08 02:01:58 +0500

    created new template project and added navigation

---
+++ App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Navigation from './src/navigation';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={[backgroundStyle, styles.container]}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <View
+        style={[
+          styles.container,
+          {
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          },
+        ]}>
+        <Navigation />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {flex: 1},
+});
